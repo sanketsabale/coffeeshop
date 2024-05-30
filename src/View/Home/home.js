e371bcb2fb7752747e3a4e2ba1235294909ef05b
tushar kothimbire <tusharkothimbire0@gmail.com>
2024-05-30 01:13:47 -0700

    home

--- src/View/Home/home.js
+++ src/View/Home/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar } from '../../Component/Navbar/Navbar';
-import { Footer } from '../../Component/Footer/';
+import { Footer } from '../../Component/Footer/Footer';
 
 
 export  function Home(){
